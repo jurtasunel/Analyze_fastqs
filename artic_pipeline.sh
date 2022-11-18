952b05c5c3de81dc1b5c39fcef0ee57880b8d084
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2022-11-18 16:35:23 +0000

    Update artic_pipeline.sh

--- artic_pipeline.sh
+++ artic_pipeline.sh
@@ -7,15 +7,17 @@
 #### Muscle conda error: https://github.com/artic-network/artic-ncov2019/issues/93
 #### Artic common issues: https://github.com/artic-network/artic-ncov2019/issues
 
-# Quality check for one barcode with artic
-artic guppyplex --min-length 300 --max-length 1400 --directory /home/josemari/Desktop/Jose/fix_fastqs/Laura_fastQs/barcode29 --prefix Laura
+#### This script requires the artic environment to be activated with "source activate artic-ncov2019".
 
-# medaka command with artic primers
-artic minion --medaka --medaka-model r941_min_high_g360 --normalise 200 --threads 8 --scheme-directory ~/artic-ncov2019/primer_schemes/midnight/ --read-file Laura_barcode29.fastq nCoV-2019/V1 Laura_barcode29
+# Get the required variables. Example:
+barcode_path="/home/josemari/Desktop/Jose/fix_fastqs/Eric_fastqs/"
+barcode="barcode06"
+prefix="Eric"
 
-# Command with arguments for loop
-artic minion --medaka --medaka-model r941_min_high_g360 --normalise 400 --threads 5 --scheme-directory ~/artic-ncov2019/primer_schemes/midnight/ --read-file ${PREFIX_RUN}_${BARCODE}.fastq nCoV-2019/V1 ${PREFIX_RUN}_${BARCODE}
+# Quality check for one barcode with artic and concatenate all fasqs in one single file.
+echo "Running artic guppyplex for ${barcode_path}${barcode}"
+artic guppyplex --min-length 300 --max-length 1400 --directory ${barcode_path}${barcode} --prefix ${prefix}
 
-
-
-
+# Run medaka with artic primersto produce consensus sequence.
+echo "Running artic medaka for ${barcode_path}${barcode}"
+artic minion --medaka --medaka-model r941_min_high_g360 --normalise 200 --threads 8 --scheme-directory ~/artic-ncov2019/primer_schemes/midnight/ --read-file ${prefix}_${barcode}.fastq nCoV-2019/V1 ${prefix}_${barcode}
